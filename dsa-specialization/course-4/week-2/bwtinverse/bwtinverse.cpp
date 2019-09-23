2323f49c61decedf2ebd07f34b1791359cf8d09f
john-echelon <johshin@gmail.com>
2019-09-22 20:01:45 -0400

    Reduced to one matrix.

--- dsa-specialization/course-4/week-2/bwtinverse/bwtinverse.cpp
+++ dsa-specialization/course-4/week-2/bwtinverse/bwtinverse.cpp
@@ -14,31 +14,30 @@ string InverseBWT(const string& bwt) {
   string result = "";
   int length = bwt.length();
   vector<string> matrix = vector<string>(length, string(length, ' '));
-  vector<string> matrix2 = vector<string>(length, string(length, ' '));
   for (int i = 0; i < length; i++) {
     matrix[i][0] = bwt[i]; 
-    matrix2[i][0] = bwt[i]; 
   }
   for (int i = 1; i <= length ; i++) {
     sort(matrix.begin(), matrix.end());
+    // cout << "sorted matrix\n";
+    // for (auto & str : matrix) {
+    //   cout << str << endl;
+    // }
+    if (i < length) {
     for (int j = 0; j < length; j++) {
-      matrix2[j][i] = matrix[j][i-1];
-        // matrix[j] = matrix2[j];
-      if (i < length) {
-        for (int k = 0; k <= i; k++) {
-          matrix[j][k] = matrix2[j][k];
-        }
+      for (int k = i; k >= 1; --k) {
+        matrix[j][k] = matrix[j][k-1];
       }
     }
+      for (int i = 0; i < length; i++) {
+        matrix[i][0] = bwt[i]; 
+      }
+    }
+    // cout << "matrix\n";
+    // for (auto & str : matrix) {
+    //   cout << str << endl;
+    // }
   }
-  // cout << "matrix1\n";
-  // for (auto & str : matrix) {
-  //   cout << str << endl;
-  // }
-  // cout << "matrix2\n";
-  // for (auto & str : matrix2) {
-  //   cout << str << endl;
-  // }
   result = matrix[0].substr(1, length-1) + '$';
   // write your code here
 
