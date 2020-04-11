499092f2538885d627c08cf15eb54feaf6114193
john-echelon <johshin@gmail.com>
2020-04-11 10:34:51 -0400

    Implemented energy_values; week 2 assignment problem 1.

--- dsa-specialization/course-5/week-2/energy_values/energy_values.cpp
+++ dsa-specialization/course-5/week-2/energy_values/energy_values.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 const double EPS = 1e-6;
 const int PRECISION = 20;
 
@@ -29,6 +31,23 @@ struct Position {
     int row;
 };
 
+void PrintColumn(const Column &column) {
+    int size = column.size();
+    std::cout.precision(PRECISION);
+    for (int row = 0; row < size; ++row)
+        std::cout << column[row] << std::endl;
+}
+
+void PrintMatrix(const Matrix &m) {
+    int size = m.size();
+    std::cout.precision(PRECISION);
+    for (int row = 0; row < size; ++row) {
+      for (int col = 0; col < size; ++col) {
+        std::cout << m[row][col] << " ";
+      }
+      std::cout << std::endl;
+    }
+}
 Equation ReadEquation() {
     int size;
     std::cin >> size;
@@ -49,10 +68,24 @@ Position SelectPivotElement(
     // This algorithm selects the first free element.
     // You'll need to improve it to pass the problem.
     Position pivot_element(0, 0);
-    while (used_rows[pivot_element.row])
+    while (used_rows[pivot_element.row]) 
         ++pivot_element.row;
     while (used_columns[pivot_element.column])
         ++pivot_element.column;
+    Position pivot_temp = pivot_element;
+    int v_size = a.size() - 1;
+    while (a[pivot_temp.row][pivot_temp.column] == 0 && pivot_temp.column < v_size) {
+      while (a[pivot_temp.row][pivot_temp.column] == 0 && pivot_temp.row < v_size) {
+          pivot_temp.row++;
+      }
+      // if rows were exhausted move to next column, and start over from original pivot row
+      if (a[pivot_temp.row][pivot_temp.column] == 0 && pivot_temp.column < v_size) {
+        pivot_temp.row = pivot_element.row;
+        pivot_temp.column++;
+      }
+    }
+    // if no appropriate pivot is found, pivot_temp will be set to (a.size() - 1, a.size() - 1)
+    pivot_element = pivot_temp;
     return pivot_element;
 }
 
@@ -65,6 +98,26 @@ void SwapLines(Matrix &a, Column &b, std::vector <bool> &used_rows, Position &pi
 
 void ProcessPivotElement(Matrix &a, Column &b, const Position &pivot_element) {
     // Write your code here
+    // Reduce row by pivot value;
+    double pivot_value = a[pivot_element.row][pivot_element.column];
+    if (pivot_value == 0) return;
+    int v_size = a.size();
+    for (int j = pivot_element.column; j < v_size; j++) {
+      a[pivot_element.row][j] /= pivot_value;
+    }
+    b[pivot_element.row] /= pivot_value;
+    // For all appropriate non-pivot rows...
+    for (int i = 0; i < v_size; i++) {
+      int targetElement = a[i][pivot_element.column];
+      if (targetElement == 0 || i == pivot_element.row)
+        continue;
+      // Subtract row by pivot row
+      for (int j = 0; j < v_size; j++) {
+        a[i][j] += (a[pivot_element.row][j] * targetElement * -1);
+      }
+      b[i] += (b[pivot_element.row] * targetElement * -1);
+    }
+
 }
 
 void MarkPivotElementUsed(const Position &pivot_element, std::vector <bool> &used_rows, std::vector <bool> &used_columns) {
@@ -81,23 +134,22 @@ Column SolveEquation(Equation equation) {
     std::vector <bool> used_rows(size, false);
     for (int step = 0; step < size; ++step) {
         Position pivot_element = SelectPivotElement(a, used_rows, used_columns);
+        cout << "Pivot Element: " << pivot_element.row << ", " << pivot_element.column << "\n";
         SwapLines(a, b, used_rows, pivot_element);
+        cout << "Pivot Element (Swap): " << pivot_element.row << ", " << pivot_element.column << "\n";
         ProcessPivotElement(a, b, pivot_element);
+        PrintMatrix(a);
+        PrintColumn(b);
         MarkPivotElementUsed(pivot_element, used_rows, used_columns);
     }
 
     return b;
 }
 
-void PrintColumn(const Column &column) {
-    int size = column.size();
-    std::cout.precision(PRECISION);
-    for (int row = 0; row < size; ++row)
-        std::cout << column[row] << std::endl;
-}
-
 int main() {
     Equation equation = ReadEquation();
+    // PrintMatrix(equation.a);
+    // PrintColumn(equation.b);
     Column solution = SolveEquation(equation);
     PrintColumn(solution);
     return 0;
