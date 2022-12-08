7ead81d949a3dcf1fd65d15a201b9345de96c66b
John Echelon <johshin@gmail.com>
2022-12-08 18:27:29 -0500

    Implemented school bus

--- dsa-specialization/course-5/week-4/school_bus/school_bus.cpp
+++ dsa-specialization/course-5/week-4/school_bus/school_bus.cpp
@@ -1,3 +1,4 @@
+// Good job! (Max time used: 0.03/1.00, max memory used: 12677120/2147483648.)
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -52,22 +53,22 @@ std::pair<int, vector<int> > optimal_path(const Matrix& graph) {
     // generate permutations of subsets of size s
     for (size_t i = 0; i < s - 1; ++i)
       p[i] = true;
-    cout << "s " << s << endl;
+    // cout << "s " << s << endl;
     do
     {
-      cout << "mask(2) ";
-      for (size_t i = 0; i < p.size(); ++i)
-        cout << p[i];
-      cout << endl;
+      // cout << "mask(2) ";
+      // for (size_t i = 0; i < p.size(); ++i)
+      //   cout << p[i];
+      // cout << endl;
       pair<int, vector<int>> info = get_mask(p, s);
       int mask = info.first;
       vector<int> subset = info.second;
-      cout << "mask(10) " << mask << endl;
+      // cout << "mask(10) " << mask << endl;
 
-      cout << "subset: ";
-      for (size_t i = 0; i < subset.size(); ++i)
-        cout << subset[i] << " ";
-      cout << endl;
+      // cout << "subset: ";
+      // for (size_t i = 0; i < subset.size(); ++i)
+      //   cout << subset[i] << " ";
+      // cout << endl;
       for (int i = 0; i < subset.size(); i++)
         for (int j = 0; j < subset.size(); j++)
         {
@@ -83,39 +84,37 @@ std::pair<int, vector<int> > optimal_path(const Matrix& graph) {
 
   vector<int> best_path;
   int best_ans = INF;
-  for (size_t i = 2; i <= n; i++)
+  int next_ans = INF;
+  int next = 1;
+
+  for (size_t i = 2; i <= n; i++) {
     best_ans = min(memo[maskSize - 1][i] + graph[i - 1][0], best_ans);
-  for (size_t i = 1; i <= n; i++)
-    cout << i << " " << memo[maskSize - 1][i] << " w " << graph[i - 1][0] << endl;
-    /*
+  }
+  // for (size_t i = 1; i <= n; i++)
+  //   cout << i << " " << memo[maskSize - 1][i] << " w " << graph[i - 1][0] << endl;
+  if (best_ans == INF)
+    return {-1, best_path};
+  next_ans = best_ans;
+  int nextMask = maskSize - 1;
+  do
+  {
+    for (size_t i = 1; i <= n; i++)
+    {
+      if (i == next)
+        continue;
+      int dist = memo[nextMask][i] + graph[i - 1][next - 1];
+      if (dist == next_ans)
+      {
+        next = i;
+        next_ans = memo[nextMask][i];
+        best_path.emplace_back(next);
+        nextMask &= ~(1 << next - 1);
+        break;
+      }
+    }
+  } while (next != 1);
 
-    do {
-        int cur_sum = 0;
-        bool ok = true;
-        for (size_t i = 1; i < n && ok; ++i)
-            if (graph[p[i - 1]][p[i]] == INF)
-                ok = false;
-            else
-                cur_sum += graph[p[i - 1]][p[i]];
-        if (graph[p.back()][p[0]] == INF)
-            ok = false;
-        else
-            cur_sum += graph[p.back()][p[0]];
-
-        if (!ok)
-            continue;
-        if (cur_sum < best_ans) {
-            best_ans = cur_sum;
-            best_path = p;
-        }
-    } while (std::next_permutation(p.begin(), p.end()));
-
-    if (best_ans == INF)
-        best_ans = -1;
-    for (size_t i = 0; i < best_path.size(); ++i)
-        ++best_path[i];
-    return std::make_pair(best_ans, best_path);
-    */
+  std::reverse(best_path.begin(), best_path.end());
   return std::make_pair(best_ans, best_path);
 }
 
